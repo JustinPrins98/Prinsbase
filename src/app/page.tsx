a37c6793c9e4d3619f72b370ade5c6f022d43ab4
Justin Prins <justin.prins@interly.nl>
2025-10-15 15:46:30 +0200

    02: Database implementation. With Prisma and neon.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-const Page = () => {
-  const something = true
 
-  return (
+const Page = () => {
+
+ return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
        <Button variant="outline">
         Click me
