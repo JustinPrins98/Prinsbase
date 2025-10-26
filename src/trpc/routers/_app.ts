e3b44d0c92841b6a943ee56fe692342993267f2e
Justin Prins <justin.prins@interly.nl>
2025-10-26 14:21:28 +0100

    07: Added Ai providers gemini, openai and anthropic

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,18 @@
 // /trpc/routers/_app.ts
 import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init'
+// Google Generative AI
+
 
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    })
+
+    return { succes: true, message: "Job queeed" }
+  }),
+
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.workflow.findMany();
 
