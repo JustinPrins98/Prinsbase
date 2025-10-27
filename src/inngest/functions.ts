97934268fe7415075e67734bed6948464e36c5df
Justin Prins <justin.prins@interly.nl>
2025-10-27 14:05:34 +0100

    08: Error tracking added Sentry for error tracking.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/db";
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai"
 import { createAnthropic } from "@ai-sdk/anthropic"
@@ -15,25 +16,43 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend", "5s")
 
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+
+
     const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text",
       generateText, {
       model: google("gemini-2.5-flash"),
       system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?"
+      prompt: "What is 2 + 2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     })
 
     const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text",
       generateText, {
       model: openai("gpt-4.1-nano"),
       system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?"
+      prompt: "What is 2 + 2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     })
 
     const { steps: anthropicSteps } = await step.ai.wrap("anthropic-generate-text",
       generateText, {
       model: anthropic("claude-3-5-haiku-20241022"),
       system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?"
+      prompt: "What is 2 + 2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     })
 
 
